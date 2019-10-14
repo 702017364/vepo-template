64fa50d50a801d88c3997aa4cfa908fc4c719d8a
fshuirong <702017364@qq.com>
2019-10-14 10:39:02 +0800

    修复 js 编译报错引起的服务退出

--- gulp/es.js
+++ gulp/es.js
@@ -4,6 +4,8 @@ import rollup from 'gulp-rollup';
 import gulpif from 'gulp-if';
 import uglify from 'gulp-uglify-es';
 import sourcemaps from 'gulp-sourcemaps';
+import plumber from 'gulp-plumber';
+import notify from 'gulp-notify';
 import setting, { mjss, dest } from './setting';
 import dev from './dev';
 import plugins from './babel-cli';
@@ -13,6 +15,9 @@ const imp = setting.import;
 export default () => {
   return gulp
     .src(mjss, { allowEmpty: true })
+    .pipe(plumber({
+      errorHandler: notify.onError('<%= error.message %>')
+    }))
     .pipe(gulpif(dev, sourcemaps.init()))
     .pipe(babel({
       babelrc: setting.babelrc,
