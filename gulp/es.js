2176b5d1b7fe3f1a5e5bb931cf7dfab17405e065
fshuirong <702017364@qq.com>
2019-10-14 11:21:38 +0800

    修复非 rollup 时，配置报错

--- gulp/es.js
+++ gulp/es.js
@@ -1,16 +1,23 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
-import rollup from 'gulp-rollup';
 import gulpif from 'gulp-if';
 import uglify from 'gulp-uglify-es';
 import sourcemaps from 'gulp-sourcemaps';
 import plumber from 'gulp-plumber';
 import notify from 'gulp-notify';
+import rollup from 'gulp-rollup';
+import through2 from 'through2';
 import setting, { mjss, dest } from './setting';
 import dev from './dev';
 import plugins from './babel-cli';
 
 const imp = setting.import;
+
+const rollup0 = () => {
+  return typeof imp == 'object'
+    ? rollup(imp)
+    : through2.obj();
+};
 
 export default () => {
   return gulp
@@ -23,7 +30,7 @@ export default () => {
       babelrc: setting.babelrc,
       plugins,
     }))
-    .pipe(gulpif(typeof imp == 'object', rollup(imp)))
+    .pipe(rollup0())
     .pipe(gulpif(!dev, uglify()))
     .pipe(gulpif(dev, sourcemaps.write('./')))
     .pipe(dest('bin') |> gulp.dest);
